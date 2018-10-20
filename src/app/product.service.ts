78d1ebe125a8c1a7ca8658dfca57e2058eff265f
Randall Theis <rtheis@outlook.com>
2018-10-20 13:29:20 -0400

    update for failing test
    code correct test is failing on spaces in code

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import {Album} from './album';
-import {Observable} from 'rxjs/Observable';
-import { Product } from './product';
+import { Observable } from 'rxjs/Observable';
+import {Product} from './product';
 
 @Injectable()
 export class ProductService {
@@ -13,11 +13,11 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
- getAlbum(id: number):  Observable<Album>  {
+ getAlbum(id: number): Observable<Album>  {
       return this._http.get(this._albumUrl).map((response) => <Album> response.json());
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map(response => <Product[]> response.json());
+    return this._http.get(this._productsUrl).map((response)=><Product[]>response.json());
   }
 }
